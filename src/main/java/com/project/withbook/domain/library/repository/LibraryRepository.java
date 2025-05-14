b8ea78b447082e966024450e6eb753ffb8e15410
thezz9 <thezz9@naver.com>
2025-05-14 18:15:07 +0900

    feat: LibraryRepository에 EntityGraph 적용한 최신순 조회 메서드 추가

--- src/main/java/com/project/withbook/domain/library/repository/LibraryRepository.java
+++ src/main/java/com/project/withbook/domain/library/repository/LibraryRepository.java
@@ -1,7 +1,14 @@
 package com.project.withbook.domain.library.repository;
 
 import com.project.withbook.domain.library.entity.Library;
+import com.project.withbook.domain.user.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LibraryRepository extends JpaRepository<Library, Long> {
+
+    @EntityGraph(attributePaths = {"user", "book"})
+    List<Library> findAllByUserOrderByUpdatedAtDesc(User user);
 }
